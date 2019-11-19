42160acc9f7f7b8474dbcdd844d99d85b53e180a
Ranran He <cangxuexiao@gmail.com>
2019-11-19 00:38:50 -0500

    service and model

---
+++ src/app/models/item.ts
@@ -0,0 +1,13 @@
+export class Item {
+    id: string;
+    title: string;
+    content: string;
+    createdDate: Date;
+    modifiedDate: Date;
+    constructor(title: string, content: string) {
+        this.title = title;
+        this.content = content;
+        this.createdDate = new Date();
+        this.modifiedDate = new Date();
+    }
+}
